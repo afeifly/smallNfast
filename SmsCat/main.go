8a0885d6b30949260f8e7c4cc7983ef83a23bdd8
ex <ex@mini4.lan>
2025-12-12 22:39:37 +0800

    fix 15

--- SmsCat/main.go
+++ SmsCat/main.go
@@ -5,6 +5,8 @@ import (
 	"embed"
 	"log"
 	"os"
+	"syscall"
+	"unsafe"
 
 	"smallNfast/internal/app"
 	"smallNfast/internal/db"
@@ -16,12 +18,58 @@ import (
 	"github.com/wailsapp/wails/v2/pkg/options/assetserver"
 	"github.com/wailsapp/wails/v2/pkg/runtime"
 	"go.uber.org/zap"
+	"golang.org/x/sys/windows"
 )
 
 //go:embed frontend/*
 var assets embed.FS
 
+var (
+	kernel32         = windows.NewLazySystemDLL("kernel32.dll")
+	procCreateMutexW = kernel32.NewProc("CreateMutexW")
+	procCloseHandle  = kernel32.NewProc("CloseHandle")
+)
+
+func createMutex(name string) (uintptr, error) {
+	ret, _, err := procCreateMutexW.Call(
+		0,
+		0,
+		uintptr(unsafe.Pointer(windows.StringToUTF16Ptr(name))),
+	)
+	if ret == 0 {
+		return 0, err
+	}
+	return ret, nil
+}
+
+func checkSingleInstance() bool {
+	mutexName := "Global\\SMSCat_SingleInstance_Mutex"
+	mutex, err := createMutex(mutexName)
+	if err != nil {
+		return false
+	}
+	
+	// Check if mutex already exists (another instance is running)
+	lastErr := windows.GetLastError()
+	if lastErr == windows.ERROR_ALREADY_EXISTS {
+		// Close the mutex handle we just got
+		if mutex != 0 {
+			procCloseHandle.Call(mutex)
+		}
+		return false // Another instance exists
+	}
+	
+	// Keep mutex open for the lifetime of the app
+	// It will be released when the process exits
+	return true // This is the first instance
+}
+
 func main() {
+	// Check for single instance
+	if !checkSingleInstance() {
+		log.Println("SMSCat is already running. Exiting...")
+		os.Exit(0)
+	}
 	// 1. Setup Logger
 	logger, _ := zap.NewProduction()
 	defer logger.Sync()
